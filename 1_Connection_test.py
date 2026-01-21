d78d931f9aafc71331a1c104eec7d616616de2e5
PedroGL <pedrodatadriven@gmail.com>
2026-01-21 15:01:40 +0000

    Add .gitignore and modify 1_Connection_test.py

--- 1_Connection_test.py
+++ 1_Connection_test.py
@@ -2,18 +2,17 @@ import os
 import snowflake.snowpark.functions
 from snowflake.snowpark import Session
 from snowflake.snowpark.functions import col
+import json
 
-connection_parameters = {"account":"ijvunnh-ny22848",
-"user":"pradeep",
-"password": "AbcdAbcdAbcd067$",
-"role":"ACCOUNTADMIN",
-"warehouse":"COMPUTE_WH",
-"database":"DEMO_DB",
-"schema":"PUBLIC"
-}
+
+with open ("config.json") as f:
+    connection_parameters = json.load(f)
 
 test_session = Session.builder.configs(connection_parameters).create()
 
 print(test_session.sql("select current_warehouse(), current_database(), current_schema()").collect())
 
 session = Session.builder.configs(connection_parameters).create()
+
+
+
